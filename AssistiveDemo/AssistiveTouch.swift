f0ddc1c9ac68bcc8caf7893da614d09af9b0c227
virendall <viru.dall@gmail.com>
2016-12-30 12:43:28 +0530

    Update AssistiveTouch.swift
    touch issue fix

--- AssistiveDemo/AssistiveTouch.swift
+++ AssistiveDemo/AssistiveTouch.swift
@@ -17,8 +17,8 @@ class AssistiveTouch: UIWindow {
         }
     }
     var isFullScreen:Bool = false;
-    var smallSize:CGRect = CGRect(x: 0, y: 100, width: 50, height: 50)
     let kScreenSize = UIScreen.main.bounds.size;
+    var smallSize:CGRect = CGRect(x: 2.5, y: 100, width: 50, height: 50)
     var touchView:UIView!
     var touchButton:UIButton!
     var panGesture:UIPanGestureRecognizer!
@@ -37,10 +37,15 @@ class AssistiveTouch: UIWindow {
     // MARK: init touch view
     
     func commonInIt(){
+        let width = 50 * (kScreenSize.width / 320.0)
+        smallSize = CGRect(x: 2.5, y: 100, width: width, height: width)
         self.touchView = UIView(frame: smallSize)
         self.touchButton = UIButton(frame:touchView.bounds)
-        self.touchButton.backgroundColor = UIColor.black
-        self.touchView.backgroundColor = UIColor.black
+        self.touchButton.clipsToBounds = true
+        self.touchButton.backgroundColor = .black
+        self.touchView.backgroundColor = .black
+        self.touchView.layer.cornerRadius = 10.0
+        self.touchView.clipsToBounds = true
         self.touchButton.addTarget(self, action: #selector(touchButtonClick(sender:)), for: .touchUpInside)
         self.touchView.addSubview(self.touchButton)
         self.touchView.isUserInteractionEnabled = true
@@ -56,10 +61,8 @@ class AssistiveTouch: UIWindow {
         if isFullScreen {
             isFullScreen = false
             self.canDrag = true
-            self.touchButton.isHidden = false
         } else{
             isFullScreen = true
-            self.touchButton.isHidden = true
             smallSize = touchView.frame
             let tviewSize = CGSize(width:kScreenSize.width-20, height: kScreenSize.width-20)
             frame = CGRect(x: (kScreenSize.width - tviewSize.width)/2, y: (kScreenSize.height - tviewSize.width)/2, width: tviewSize.width, height: tviewSize.height)
@@ -67,8 +70,9 @@ class AssistiveTouch: UIWindow {
         }
         UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
             self.touchView.frame = frame
-        }, completion: nil)
-        
+            },completion: { (complete) in
+                self.touchButton.frame = self.touchView.bounds
+            })
     }
     func showAssistiveTouch(){
         self.isHidden = false
@@ -97,9 +101,9 @@ class AssistiveTouch: UIWindow {
         case .cancelled, .ended:
             var center = self.touchView.center;
             if (self.touchView.center.x > (kScreenSize.width/2)){
-                center.x = kScreenSize.width - self.touchView.frame.size.width/2;
+                center.x = kScreenSize.width - self.touchView.frame.size.width/2 - 2.5;
             }else{
-                center.x = self.touchView.frame.size.width/2;
+                center.x = self.touchView.frame.size.width/2 + 2.5;
             }
             UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
                 self.touchView.center = center;
@@ -123,15 +127,9 @@ class AssistiveTouch: UIWindow {
         var allowTouch:Bool = false;
         if self.touchView.frame.contains(point) {
             allowTouch = true;
-//            if(isFullScreen){
-//                self.touchButtonClick(sender: self.touchButton)
-//            }
-        }else{
-            self.touchButtonClick(sender: self.touchButton)
-        }
-        if (self.rootViewController != nil)
-        {
+        } else if(isFullScreen){
             allowTouch = true;
+            self.touchButtonClick(sender: self.touchButton)
         }
         return allowTouch
     }
